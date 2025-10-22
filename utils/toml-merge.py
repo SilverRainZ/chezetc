e54b44e724877c5bd7b9e6c052644f2f9b53a613
Shengyu Zhang <i@silverrainz.me>
2025-10-23 00:40:26 +0800

    fix: Deep merge toml config

--- utils/toml-merge.py
+++ utils/toml-merge.py
@@ -13,6 +13,20 @@ def load(fn):
     else:
         return {}
 
+
+def merge(src, dst):
+    """Recursively merge two dicts instead of using ``d1 | d2``"""
+    for key, value in src.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = dst.setdefault(key, {})
+            merge(value, node)
+        else:
+            dst[key] = value
+
+    return dst
+
 a = load(sys.argv[1])
 b = load(sys.argv[2])
-print(tomli_w.dumps(a | b))
+merge(a, b)
+print(tomli_w.dumps(b))
